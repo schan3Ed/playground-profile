4038bd1e5bc7318249e20719479aee11b738f229
Ed Chan <schan3edd@gmail.com>
2024-06-26 00:49:19 -0400

    card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import Image from "next/image";
 import Profile from "@/components/profile";
+
+import { Button } from "@material-tailwind/react";
+import React from "react";
 
 export default function Home() {
   return (
--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import React from "react";
-import Location from "@/components/location";
+import Location from "./location";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
 
 interface ProfileProps {
   Name: string;
@@ -11,21 +19,42 @@ interface ProfileProps {
 
 const Profile: React.FC<ProfileProps> = (props: ProfileProps) => {
   return (
-    <div className="dark:bg-gray-900 rounded-lg overflow-hidden shadow-lg py-5 px-32 text-center w-128">
-      <Image
-        src="/images/myhead.png"
-        width={500}
-        height={500}
-        alt="my face"
-        className="h-32 w-32 rounded-full border-dashed border-2 border-orange-500 mx-auto my-10"
-      ></Image>
-      <div className="mb-10">
-        <h3 className="text-2xL dark:text-white mb-2">
-          {props.Name} @ {props.Company}
-        </h3>
-        <Location state="NC" city="Raleigh" />
-      </div>
-    </div>
+    <Card color="gray" className="shadow-lg text-center w-96">
+      <CardBody>
+        <img
+          src="/images/myhead.png"
+          alt="my face"
+          className="w-36 rounded-full outline-dashed outline-4 outline-orange-300 mx-auto my-10 outline-offset-8 "
+        />
+        <div className="grid grid-row-3">
+          <Typography variant="h4" color="white">
+            {props.Name}
+          </Typography>
+          <Typography variant="small" color="gray">
+            Senior Software Engineer
+          </Typography>
+          <div className="mt-4">
+            <Location state="NC" city="Raleigh" />
+          </div>
+        </div>
+      </CardBody>
+      <CardFooter className="grid grid-cols-2 mx-auto gap-10 mb-8">
+        <a href="https://www.linkedin.com/in/schan3ed/">
+          <img
+            src="/icons/linkedin.svg"
+            className="w-8 rounded-lg invert mx-auto"
+            alt="LinkedIn"
+          />
+        </a>
+        <a href="https://github.com/schan3Ed">
+          <img
+            src="/icons/github.svg"
+            className="w-8 rounded-lg invert mx-auto"
+            alt="Github"
+          />
+        </a>
+      </CardFooter>
+    </Card>
   );
 };
 
